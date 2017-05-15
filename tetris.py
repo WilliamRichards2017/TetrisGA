2f6c72e944bcef2ba9109768b0d9db4cba64eaf3
Will Richards <richardsw@carleton.edu>
2017-05-14 20:58:09 -0500

    fixed tetraminos

--- tetris.py
+++ tetris.py
@@ -145,18 +145,18 @@ tetraminos = [[[[0,0,0,0,0],
                 [0,0,0,0,0]]]]
 
 def get_tetramino():
-    global x
-    x = randint(0, 7)
+    return randint(0, 7)
 
 
-def rotate_tetramino():
-    global y
-    y = (y + 1) % 4
+def rotate_tetramino(rotation):
+    return rotation % 4
 
 
 def remove_row():
     return null
 
+def get_final_piece(block, rotation):
+    return tetraminos[block][rotation]
 
 get_tetramino()
 rotate_tetramino()
