9ce7adea839c2f097c72cb3bbfdcf4fa3549f951
richardsw <richardsw@carleton.edu>
2017-05-24 12:24:23 -0500

    genetic changes

--- genetic.py
+++ genetic.py
@@ -2,6 +2,7 @@ import MultiNEAT as NEAT
 import numpy as np
 import math
 import random
+import csv
 from Pieces import *
 from Board import *
 
@@ -15,31 +16,10 @@ params = NEAT.Parameters()
 params.PopulationSize = popSize
 params.RouletteWheelSelection = True
 
-'''
-Create a genome with 310 inputs and 2 outputs
-Inputs consist of
-    - Board Configuration: 10 x 22= 220
-    - Side Buffer: 88
-    - Bias: 1
-    - Piece: 1
-Outputs are
-    - Number of rotations
-    - Horizontal displacement
-'''
-genome = NEAT.Genome(0, 223, 30, 2, False, NEAT.ActivationFunction.UNSIGNED_SIGMOID, NEAT.ActivationFunction.UNSIGNED_SIGMOID, 0, params)
-
-params.DynamicCompatibility = True
-params.CompatTreshold = 2.0
-params.YoungAgeTreshold = 15
-params.SpeciesMaxStagnation = 100
-params.OldAgeTreshold = 35
-params.MinSpecies = 5
-params.MaxSpecies = 10
-params.RouletteWheelSelection = False
 
 params.MutateRemLinkProb = 0.02
 params.RecurrentProb = 0
-params.OverallMutationRate = 0.15
+params.OverallMutationRate = 0.50
 params.MutateAddLinkProb = 0.08
 params.MutateAddNeuronProb = 0.01
 params.MutateWeightsProb = 0.90
@@ -67,13 +47,58 @@ params.ActivationFunction_SignedSine_Prob = 1.0
 params.ActivationFunction_UnsignedSine_Prob = 0.0
 params.ActivationFunction_Linear_Prob = 1.0
 
+params.DivisionThreshold = 0.5
+params.VarianceThreshold = 0.03
+params.BandThreshold = 0.3
+params.InitialDepth = 2
+params.MaxDepth = 3
+params.IterationLevel = 1
+params.Leo = False
+params.GeometrySeed = False
+params.LeoSeed = False
+params.LeoThreshold = 0.3
+params.CPPN_Bias = -1.0
+params.Qtree_X = 0.0
+params.Qtree_Y = 0.0
+params.Width = 1.
+params.Height = 1.
+params.Elitism = 0.1
+
+
+
+
+'''
+Create a genome with 310 inputs and 2 outputs
+Inputs consist of
+    - Board Configuration: 10 x 22= 220
+    - Side Buffer: 88
+    - Bias: 1
+    - Piece: 1
+Outputs are
+    - Number of rotations
+    - Horizontal displacement
+'''
+genome = NEAT.Genome(0, 310, 30, 2, False, NEAT.ActivationFunction.UNSIGNED_SIGMOID, NEAT.ActivationFunction.UNSIGNED_SIGMOID, 0, params)
+
+params = NEAT.Parameters()
+params.PopulationSize = 20
+params.DynamicCompatibility = True
+
+
+
+
+
 
 pop = NEAT.Population(genome, params, True, 1.0, 0)
+substrate = [(i/width , i % width)  for i in range(width * height)]
+piecearray = [(i/5 , i % 5)  for i in range(25)]
+substrate.extend(piecearray * 4)
+substrate = NEAT.Substrate( substrate, [], [(2, 11), (0, 3)])
 
 def evaluate(genome):
     # create a neural network for the genome
     net = NEAT.NeuralNetwork()
-    genome.BuildPhenotype(net)
+    genome.BuildESHyperNEATPhenotype(net, substrate, params)
     
     # create a board
     board = Board(width, height)
@@ -90,6 +115,10 @@ def evaluate(genome):
         
         # pass board configuation & piece to the neural network
         conf = board.getBoard().ravel()
+        c2, c3, c4, c5 = [piece.getPieceArray(i).ravel() for i in range(4)]
+        conf = np.concatenate([conf, c2, c3, c4, c5])
+        
+        #conf = [tuple(row) for row in conf]
         np.append(conf, pieceNum)
         np.append(conf, 1.0)    # bias
         
@@ -97,14 +126,20 @@ def evaluate(genome):
         net.Activate()
         output = net.Output()
         
-        col = int(math.ceil(output[0] * 9 + 2))
-        rot = int(math.ceil(output[1] * 3))
+        
+        print "output:", output[0], "outout", output[1]
+        ## Found bug, output isnt always between 0 and 1
+        col = int((output[0]*8 %8) + 2) 
+        rot = int(output[1]*3 % 3)
+        
+        ##print "output1:", output[0], "output2:", output[1]
 
         # update the board by the output we get
-        print "rot: ", rot, "col: ", col
+        ##print "rot: ", rot, "col: ", col
         canAddPiece = board.addPiece(piece, rot, col)
         line = line + board.clearRows()
         if canAddPiece == -1:
+            #print board
             break
         current += 1
         
@@ -116,18 +151,18 @@ def evaluate(genome):
     
 def main():
     # for as many as maxGeneration
-    for generation in range(1):
+    for generation in range(50):
         
         # retrieve a list of all genomes in the population
         genome_list = NEAT.GetGenomeList(pop)
         
         # apply the evaluation function to all genomes
-        sum = 0.0
+        sum = 0
         for genome in genome_list:
             fitness = evaluate(genome)
             genome.SetFitness(fitness)
             sum += fitness
-        avg = sum/popSize
+        avg = sum/float(popSize)
         print avg
         # at this point we may output some information regarding the progress of evolution, best fitness, etc.
         # it's also the place to put any code that tracks the progress and saves the best genome or the entire
